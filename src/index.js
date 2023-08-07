57d1c65c3bae10cf4cfa0005c44ab86f4b1458c1
zelvad <vladislav.zelenyk@scalemeup.com>
2023-08-07 15:16:24 +0300

    fix: fix translate title

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ export default class TitleWitchDescription {
   static get toolbox() {
     return {
       icon: ToolboxIcon,
-      title: this.api.i18n.t('Заголовок / Опис')
+      title: 'Заголовок / Опис'
     };
   }
 
